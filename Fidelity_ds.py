c85f932da21faf89263bc0b1812df5cb29fdd86e
Lakshay Agarwal <633lakshay@gmail.com>
2025-02-04 09:31:43 +0530

    modified:   Fidelity_ds.py

--- Fidelity_ds.py
+++ Fidelity_ds.py
@@ -1,3 +1,3 @@
 def segregate(l):
-    """This function returns list with only integers. It takes a list as argument"""
+    """This function returns list with only integers. It takes a list as argument"""    # this is the documentation to help users
     return [i for i in l if isinstance(i, int)]
