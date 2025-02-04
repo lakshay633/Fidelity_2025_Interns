d17da4e85205fbbf86b91e4c738ec748a4c7cd35
Lakshay Agarwal <633lakshay@gmail.com>
2025-02-04 10:12:54 +0530

    new file:   Customer_class.py

---
+++ Customer_class.py
@@ -0,0 +1,24 @@
+class Customer:
+    name=""
+    balance=0
+    def __init__(self, name, bal):
+        self.name=name
+        self.balance=bal
+    def deposit(self,amount):
+        self.balance=self.balance+amount
+        print("Money Deposited Successfully")
+        print("Updated Balance is:",self.balance)
+    def withdraw(self,amount):
+        if amount>self.balance:
+            print("ERROR: Insufficient Balance")
+            return
+        self.balance=self.balance-amount
+        print("Money Withdrawn Successfully")
+        print("Updated Balance is:",self.balance)
+    def display(self):
+        print("Balance is:",self.balance)
+        
+c1=Customer("Lakshay",100000)
+c1.display()
+c1.withdraw(50000)
+c1.deposit(150000)
